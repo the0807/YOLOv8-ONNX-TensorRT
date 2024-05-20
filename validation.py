b1321117cdd4fee29beafb97b1be8d5dd8b09ac7
Eom, Taehyun <73097985+the0807@users.noreply.github.com>
2024-05-20 18:03:50 +0900

    Add files via upload

--- validation.py
+++ validation.py
@@ -5,13 +5,14 @@ import pathlib
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='validation model')
     parser.add_argument('--model', type=str, default='model/yolov8n.engine', required=True, help='Path to the .pt')
-    parser.add_argument('--q', type=str, default='fp16', required=True, help='[fp16, int8]')
+    parser.add_argument('--q', type=str, default='fp16', required=False, help='[fp16, int8]')
     parser.add_argument('--data', type=str, default='coco.yaml', required=True, help='Dataset')
     args = parser.parse_args()
     
     model = YOLO(args.model, task='detect')
     
     file_extension = pathlib.Path(args.model).suffix
+
     if file_extension == '.engine':
         if args.q == 'fp16':
             results = model.val(
@@ -41,5 +42,12 @@ if __name__ == '__main__':
             )
         else:
             print('\n[ERROR] ONNX supports only FP16 quantization!')
+    elif file_extension == '.pt':
+        results = model.val(
+            data=args.data,
+            batch=1,
+            imgsz=640,
+            verbose=False,
+        )
     else:
-        print('\n[ERROR] File extensions only support [.engine], [.onnx]')
+        print('\n[ERROR] File extensions only support [.engine], [.onnx], [.pt]')
